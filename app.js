97f3715872f3b9bfa290780b64d1510cccff9388
shivakumar <shivamahajan055@gmail.com>
2025-03-25 14:24:39 +0530

    Adding security using bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const cors = require("cors");
 const User = require("./models/user");
 const sequelize = require("./utils/database");
-const { where } = require("sequelize");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -17,18 +17,20 @@ app.get("/", (req, res) => {
 
 //signup
 app.post("/user/signup", async (req, res) => {
-  const { username, email, password } = req.body;
-
-  // Check if any field is missing
-  if (!username || !email || !password) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-
   try {
+    const { username, email, password } = req.body;
+
+    // Check if any field is missing
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     const user = await User.create({
       name: username,
       email: email,
-      password: password,
+      password: hashedPassword,
     });
     res.status(201).json({ message: "User created successfully!", user });
   } catch (error) {
@@ -42,23 +44,28 @@ app.post("/user/signup", async (req, res) => {
 });
 
 app.post("/user/login", async (req, res) => {
-  let { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({ message: "All feilds are required!" });
-  }
   try {
-    const user = await User.findOne({ where: { email: email } });
+    let { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All feilds are required!" });
+    }
+
+    const user = await User.findOne({ where: { email } });
     if (!user) {
       return res
         .status(404)
         .json({ message: "User not found! Please sign up." });
-    } else {
-      if (user.password !== password) {
-        return res.status(401).json({ message: "User not authorized!" });
-      }
     }
-    return res.status(200).json({ message: "User Login successful!", user });
+
+    // Correct async/await bcrypt.compare usage
+    const result = await bcrypt.compare(password, user.password);
+    if (!result) {
+      return res.status(401).json({ message: "Incorrect password!" });
+    }
+    return res
+      .status(200)
+      .json({ success: true, message: "User loggedin successfully!" });
   } catch (error) {
     return res
       .status(500)
@@ -67,7 +74,7 @@ app.post("/user/login", async (req, res) => {
 });
 
 sequelize
-  .sync()
+  .sync({ force: false })
   .then((result) => {
     // console.log(result);
   })
